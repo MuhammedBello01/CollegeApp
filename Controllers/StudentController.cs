c55d02e4859bf14597205cc828fc3a0164dfca7a
hp\EliteBOOK <muhammedbello1005@gmail.com>
2024-08-19 13:24:56 +0100

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,61 @@
+﻿using ColleegeApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ColleegeApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [Route("All", Name = "GetAllstudents")]
+        public ActionResult<IEnumerable<Student>> GetAllStudents()
+        {
+            return Ok(CollegeRepository.Students);
+        }
+
+        [HttpGet]
+        [Route("{id}", Name = "GetStudentById")]
+        public ActionResult<Student?> GetStudentById(int id)
+        {
+            if (id <= 0)
+                return BadRequest();
+
+            var student = CollegeRepository.Students?.FirstOrDefault(s => s?.Id == id);
+            if (student == null)
+                return NotFound($"Student with id {id} not found");
+
+            return Ok(student);
+        }
+
+        [HttpGet]
+        [Route("name", Name = "GetStudentByName")]
+        public ActionResult<Student?> GetStudentById(string Name)
+        {
+            if (string.IsNullOrEmpty(Name))
+                return BadRequest();
+
+            var student = CollegeRepository.Students?.FirstOrDefault(s => string.Equals(s.StudentName, Name, StringComparison.OrdinalIgnoreCase));
+            if (student == null)
+                return NotFound($"Student with id {Name} not found");
+
+            return Ok(student);
+        }
+
+        [HttpDelete]
+        [Route("{Id}", Name = "DeleteStudentById")]
+        public ActionResult<bool> DeleteStudentById(int id)
+        {
+            if (id <= 0)
+                return BadRequest();
+
+            var studentToDelete = CollegeRepository.Students?.FirstOrDefault(s => s?.Id == id);
+
+            if (studentToDelete == null)
+                return NotFound($"Student with id {id} not found");
+
+            CollegeRepository.Students?.Remove(studentToDelete);
+            return Ok(true);
+        }
+    }
+}
---
+++ Models/CollegeRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ColleegeApp.Models
+{
+    public static class CollegeRepository
+    {
+        public static List<Student> Students = new List<Student>()
+        {
+            new Student() { StudentName = "Moh", Address = "My house", Email = "Slymmd@gmail.com", Id = 1 },
+            new Student() { StudentName = "Cashhoo", Address = "Google it", Email = "Cashoo@gmail.com", Id = 2 }
+        };
+    }
+}
