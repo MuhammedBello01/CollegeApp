8ab540ae665c721f8656fa3b4a8765322b7c51a0
suleiman Muhammed Bello <muhammed.suleiman@wemabank.com>
2024-08-25 17:56:27 +0100

    Added Serilog and inbuilt loggers

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
 using CollegeApp.MyLogging;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Logging.  by default all loggers are active
-builder.Logging.ClearProviders(); //clearing all loggers
-builder.Logging.AddConsole(); //logging to console alone
-builder.Logging.AddDebug(); 
+// builder.Logging.ClearProviders(); //clearing all loggers
+// builder.Logging.AddConsole(); //logging to console alone
+// builder.Logging.AddDebug(); 
+
+
+Log.Logger = new LoggerConfiguration()
+   // .WriteTo.File("Log/log.txt")//this is if u want to log everything into one file
+    .WriteTo.File("Log/log.txt", rollingInterval: RollingInterval.Day)// if u want to log into new file everyday
+    .MinimumLevel.Information()
+    .CreateLogger();
+
+//builder.Host.UseSerilog();// if u want to use only serilog
+builder.Logging.AddSerilog();// if u want to use both inbuilt loggers and serilog
 
 // Add services to the container.
 
