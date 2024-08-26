8f545470dd2d7bc49c19d3c74a28e8bba5baa00a
suleiman Muhammed Bello <muhammed.suleiman@wemabank.com>
2024-08-26 09:12:43 +0100

    small update

--- Data/CollegeDbContext.cs
+++ Data/CollegeDbContext.cs
@@ -3,6 +3,6 @@ using Microsoft.EntityFrameworkCore;
 namespace CollegeApp.Data;
 
 public class CollegeDbContext : DbContext
-{
-    private DbSet<Student> Students { get; set; }
+{ 
+    DbSet<Student> Students { get; set; }
 }
